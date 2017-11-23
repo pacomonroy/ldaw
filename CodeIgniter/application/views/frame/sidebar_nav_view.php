41da1393a39dcb89f37934031020d61f1859bf84
pacomonroy <pacomonroyu@hotmail.com>
2017-11-23 09:59:36 -0600

    CodeIgniter_Tutorial

---
+++ CodeIgniter/application/views/frame/sidebar_nav_view.php
@@ -0,0 +1,27 @@
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li>
+                            <?php echo '<p class="welcome"><b> <text style="font-size:150%;"></text> <i>Bienvenido: </i></br> ' . $this->session->userdata('name') . "!</b></p>"; ?>
+                        </li>
+                        <li>
+                            <a href="<?=base_url()?>"><i class="fa fa-home fa-fw"></i> Inicio</a>
+                        </li>
+                        <?php if($this->session->userdata('role') == 'admin'): ?>
+                            <li>
+                                <a href="#"><i class="fa fa-user fa-fw"></i> Administrator<span class="fa arrow"></span></a>
+                                <ul class="nav nav-second-level">
+                                    <li> <a href="<?=base_url('admin/user_list')?>">&raquo; User List</a> </li>
+                                    <li> <a href="<?=base_url('admin/activity_log')?>">&raquo; Activity Log</a> </li>
+                                </ul>
+                            </li>
+                        <?php endif; ?>
+
+
+
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
